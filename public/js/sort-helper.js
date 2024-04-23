c6b0d612e8e76ca83112997de45fe53732a9d59b
Bpost129 <Bpost129@yahoo.com>
2024-04-23 09:56:13 -0400

    attempt sort feature

---
+++ public/js/sort-helper.js
@@ -0,0 +1,24 @@
+const sortSelect = document.getElementById('sort-select')
+// const likes = document.getElementById('likes')
+
+sortSelect.addEventListener('change', sort)
+
+function sort(arr) {
+  
+  if (sortSelect.value === 'Popular') {
+    console.log("popular")
+    arr.sort((a, b) => {
+      (a.likes.length - a.dislikes.length) - (b.likes.length - b.dislikes.length)
+    })
+  } else {
+    console.log("recent")
+    arr.reverse()
+  }
+    // posts.sort((a, b) => {
+    //   (a.likes - a.dislikes) - (b.likes - b.dislikes)
+    // })
+  // } else {
+  //   posts.reverse()
+  // }
+  
+}
